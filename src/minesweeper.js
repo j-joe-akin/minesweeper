671646d8299b54ec48d0961a44a8c00c2484d68b
j-joe-akin <jjfour@gmail.com>
2018-05-24 09:46:36 -0400

    Third commit

---
+++ src/minesweeper.js
@@ -0,0 +1,14 @@
+// defining a constant to describe what the board looks like
+const blankLine = '  |   |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// defining constant to describe what a line in the board will look like after a guess is made or bomb revealed
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
